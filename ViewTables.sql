6cca0aeaf648d974f18fc0a273abe344f9d5c11e
reverse <mckinnonr03@gmail.com>
2023-11-06 19:26:57 -0500

    add view

--- ViewTables.sql
+++ ViewTables.sql
@@ -12,3 +12,15 @@ from orders
 right join pizza on OrderID=PizzaOrdID
 group by PizzaBaseSize, PizzaBaseCrustType, Month
 order by Profit desc
+
+CREATE VIEW ProfitByOrderType AS 
+select OrderType customerType, 
+	DATE_FORMAT(OrderDate, '%Y-%m') 'Order Month', 
+	SUM(OrderCustPrice) 'TotalOrderPrice', 
+	SUM(OrderBusinessPrice) 'TotalOrderCost', 
+	SUM(OrderCustPrice)-SUM(OrderBusinessPrice) 'Profit'
+from orders
+group by customerType, 'Order Month'
+UNION
+select NULL, "Grand Total", SUM(OrderCustPrice), SUM(OrderBusinessPrice), SUM(OrderCustPrice)-SUM(OrderBusinessPrice)
+from orders
