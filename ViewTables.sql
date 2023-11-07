a456abdb060b353c0bdc957ae1742a84dab99fd4
reverse <mckinnonr03@gmail.com>
2023-11-06 19:17:09 -0500

    fix views

--- ViewTables.sql
+++ ViewTables.sql
@@ -1,5 +1,14 @@
-select ToppingName, IF(sum(val) > 0, sum(val), 0) ToppingCount from topping
+CREATE VIEW ToppingPopularity AS 
+select ToppingName Topping, IF(sum(val) > 0, sum(val), 0) ToppingCount from topping
 left join (select Pizza_ToppingToppingName, IF(Pizza_ToppingExtra=true, 2, 1) val from pizza_topping) x
 on ToppingName=Pizza_ToppingToppingName
 group by ToppingName
 order by ToppingCount desc;
+
+CREATE VIEW ProfitByPizza AS
+SELECT PizzaBaseSize, PizzaBaseCrustType, SUM(PizzaBaseCostC)-SUM(PizzaBaseCostB) Profit, 
+DATE_FORMAT(OrderDate, '%Y-%m') Month 
+from orders
+right join pizza on OrderID=PizzaOrdID
+group by PizzaBaseSize, PizzaBaseCrustType, Month
+order by Profit desc
