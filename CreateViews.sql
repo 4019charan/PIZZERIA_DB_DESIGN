5e1fa4d91c33a98344c2bd411585d656c6edba05
reverse <mckinnonr03@gmail.com>
2023-12-05 19:02:18 -0500

    fix views

--- CreateViews.sql
+++ CreateViews.sql
@@ -2,6 +2,7 @@
 
 USE PIZZERIA;
 
+
 CREATE VIEW ToppingPopularity AS 
 select ToppingName Topping, IF(sum(val) > 0, sum(val), 0) ToppingCount from topping
 left join (select Pizza_ToppingToppingName, IF(Pizza_ToppingExtra=true, 2, 1) val from pizza_topping) x
@@ -14,7 +15,7 @@ SELECT PizzaBaseSize, PizzaBaseCrustType, SUM(PizzaBaseCostC)-SUM(PizzaBaseCostB
 DATE_FORMAT(OrderDate, '%Y-%m') Month 
 from orders
 right join pizza on OrderID=PizzaOrdID
-group by PizzaBaseSize, PizzaBaseCrustType, Month
+group by PizzaBaseSize, PizzaBaseCrustType
 order by Profit desc;
 
 CREATE VIEW ProfitByOrderType AS 
@@ -24,7 +25,13 @@ select OrderType customerType,
 	SUM(OrderBusinessPrice) 'TotalOrderCost', 
 	SUM(OrderCustPrice)-SUM(OrderBusinessPrice) 'Profit'
 from orders
-group by customerType, 'Order Month'
+group by customerType, DATE_FORMAT(OrderDate, '%Y-%m')
 UNION
 select NULL, "Grand Total", SUM(OrderCustPrice), SUM(OrderBusinessPrice), SUM(OrderCustPrice)-SUM(OrderBusinessPrice)
-from orders
+from orders;
+
+select * from ToppingPopularity;
+
+select * from ProfitByPizza;
+
+select * from ProfitByOrderType;
--- ViewTables.sql
+++ ViewTables.sql
@@ -2,8 +2,15 @@
 
 USE PIZZERIA;
 
-select * from ToppingPopularity;
-
-select * from ProfitByPizza;
-
-select * from ProfitByOrderType;
+SELECT * FROM customer;
+SELECT * FROM orders;
+SELECT * FROM dinein;
+SELECT * FROM delivery;
+SELECT * FROM pickup;
+SELECT * FROM base;
+SELECT * FROM pizza;
+SELECT * FROM topping;
+SELECT * FROM pizza_topping;
+SELECT * FROM discount;
+SELECT * FROM pizza_d;
+SELECT * FROM order_d;
